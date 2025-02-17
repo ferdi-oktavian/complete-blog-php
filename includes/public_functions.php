480f961ad1761b5745e7246edaba651dad19b0eb
ferrdy <muhammadferdisp33@gmail.com>
2025-02-17 22:19:16 +0700

    edit#1

--- includes/public_functions.php
+++ includes/public_functions.php
@@ -24,4 +24,34 @@ function getPostTopic($post_id) {
 
     return $topic;
 }
+
+
+function getPublishedPostsByTopic($topic_id) {
+    global $conn; 
+    $sql = "SELECT * FROM posts ps
+                    WHERE ps.id IN
+                    (SELECT pt.post_id FROM post_topic pt
+                        WHERE pt.topic_id=$topic_id GROUP BY pt.post_id
+                        HAVING COUNT(1) = 1)";
+
+    $result = mysqli_query($conn, $sql);
+    $posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    $final_posts = array();
+    foreach ($posts as $post) {
+        $post['topic'] = getPostTopic($post['id']);
+        array_push($final_posts, $post);
+    }
+    return $final_posts;
+}
+
+function getTopicNameById($id)
+{
+    global $conn;
+    $sql = "SELECT name FROM topics WHERE id=$id";
+    $result = mysqli_query($conn, $sql);
+    $topic = mysqli_fetch_assoc($result);
+    return $topic ['name'];
+}
+
 ?>
